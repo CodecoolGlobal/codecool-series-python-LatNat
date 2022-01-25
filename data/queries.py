621373e79d54e014aa5ac970ea6408424a30390c
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-25 13:39:27 +0100

    Add functioning query to get top rated shows

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,21 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_top_rated():
+    query = '''
+        SELECT
+           shows.title,
+           shows.year,
+           shows.runtime,
+           ROUND( rating, 1),
+           STRING_AGG( name, ',' ORDER BY name) genres,
+           COALESCE( shows.trailer, 'No URL' )
+        FROM shows
+        INNER JOIN show_genres sg on shows.id = sg.show_id
+        INNER JOIN genres g on sg.genre_id = g.id
+        GROUP BY  title, year, runtime, rating, trailer
+        ORDER BY shows.rating DESC;
+        '''
+    return data_manager.execute_select(query)
