7234dbac9d391d4bf8beed980e058a846d7dfd7a
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-25 18:58:20 +0100

    Add links for titles (pointing nowhere at the moment)

--- data/queries.py
+++ data/queries.py
@@ -7,19 +7,20 @@ def get_shows():
 
 def get_top_rated():
     query = '''
-        SELECT
-           shows.title,
-           shows.year,
-           shows.runtime,
-           ROUND( rating, 1 ),
-           STRING_AGG( name, ', ' ORDER BY name ) genres,
-           COALESCE( shows.trailer, 'No URL' ) as trailer,
-           COALESCE( shows.homepage, 'No URL' ) as homepage
-        FROM shows
-        INNER JOIN show_genres sg on shows.id = sg.show_id
-        INNER JOIN genres g on sg.genre_id = g.id
-        GROUP BY  title, year, runtime, rating, trailer, homepage
-        ORDER BY shows.rating DESC
-        LIMIT 15;
+            SELECT
+                shows.id,
+                shows.title,
+                shows.year,
+                shows.runtime,
+                ROUND( rating, 1),
+                STRING_AGG( name, ',' ORDER BY name) genres,
+                COALESCE( shows.trailer, 'No URL' ) as trailer,
+                COALESCE( shows.homepage, 'No URL' ) as homepage
+            FROM shows
+            INNER JOIN show_genres sg on shows.id = sg.show_id
+            INNER JOIN genres g on sg.genre_id = g.id
+            GROUP BY  shows.id, title, year, runtime, rating, trailer, homepage
+            ORDER BY shows.rating DESC
+            LIMIT 15;
         '''
     return data_manager.execute_select(query)
