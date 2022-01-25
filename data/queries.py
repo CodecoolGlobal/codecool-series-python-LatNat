6aafcc56152284e1a72d1bbb94a2e163a59830e0
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-25 16:08:31 +0100

    Add homepage column for top rated query

--- data/queries.py
+++ data/queries.py
@@ -11,13 +11,14 @@ def get_top_rated():
            shows.title,
            shows.year,
            shows.runtime,
-           ROUND( rating, 1),
-           STRING_AGG( name, ',' ORDER BY name) genres,
-           COALESCE( shows.trailer, 'No URL' ) as trailer
+           ROUND( rating, 1 ),
+           STRING_AGG( name, ',' ORDER BY name ) genres,
+           COALESCE( shows.trailer, 'No URL' ) as trailer,
+           COALESCE( shows.homepage, 'No URL' ) as homepage
         FROM shows
         INNER JOIN show_genres sg on shows.id = sg.show_id
         INNER JOIN genres g on sg.genre_id = g.id
-        GROUP BY  title, year, runtime, rating, trailer
+        GROUP BY  title, year, runtime, rating, trailer, homepage
         ORDER BY shows.rating DESC;
         '''
     return data_manager.execute_select(query)
