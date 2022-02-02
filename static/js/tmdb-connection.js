69b41b144fcaa0f21e36a8794ca1ae640519d80a
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-02-02 09:34:16 +0100

    Fix posters

--- static/js/tmdb-connection.js
+++ static/js/tmdb-connection.js
@@ -4,16 +4,20 @@ const postersUrlBase = 'https://image.tmdb.org/t/p/original'
 
 
 async function getPosterUrl(show) {
-    const firstMatch = 0;
-    const jpegPath = await findShow(show);
-    return postersUrlBase + jpegPath.results[firstMatch].poster_path;
+    const allMatches = await findShow(show);
+    for (let match of allMatches.results) {
+        if (show === match.name) {
+            let jpegPath = match.poster_path;
+            return postersUrlBase + jpegPath
+        }
+    }
+    console.log("poster not found");
 }
 
 async function findShow(show) {
-    const showObject = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=337ddeb9751222710ad6dd2bf5794684&query=${show}`);
+    const showObject = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=337ddeb9751222710ad6dd2bf5794684&query=${show}&language=en-US&page=1`);
     return await showObject.json();
 }
-
 function createImageTag() {
     const image = document.createElement("img");
     image.className = "poster col col-third";
