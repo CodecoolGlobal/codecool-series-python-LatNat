cb062d8d1a14768ccb69358fbd410471100fde24
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-02-07 15:54:23 +0100

    Add default option and table for filter actors

--- static/js/controller.js
+++ static/js/controller.js
@@ -4,7 +4,7 @@ function selectionEventHandler() {
     const dropDownMenu = document.querySelectorAll("option");
     for (let option of dropDownMenu) {
         option.addEventListener("click", async (e) => {
-            clearTable()
+            clearTable();
             const selection = e.target.value;
             let response = await dataHandler.getActorsByGenre(selection);
             buildTableFromResponse(response);
@@ -15,7 +15,7 @@ function selectionEventHandler() {
 function clearTable() {
     let tBody = document.querySelector("tbody");
     while (tBody.hasChildNodes()) {
-        tBody.removeChild(tBody.lastChild)
+        tBody.removeChild(tBody.lastChild);
     }
 }
 
@@ -26,20 +26,23 @@ function buildTableFromResponse(response) {
         let tableCell = document.createElement("td");
         tableCell.innerText = data.name;
         tableRow.appendChild(tableCell);
-        tBody.appendChild(tableRow)
+        tBody.appendChild(tableRow);
     }
 }
 
 
 selectionEventHandler();
 window.onload = async function(){
-    let response = await dataHandler.getActorsByGenre(1)
+    clearTable();
+    const selection = await document.querySelector("select");
+    console.log(selection)
+    let response = await dataHandler.getActorsByGenre(selection.value)
     let tBody = document.querySelector("tbody");
     for (let data of response) {
         let tableRow = document.createElement("tr");
         let tableCell = document.createElement("td");
         tableCell.innerText = data.name;
         tableRow.appendChild(tableCell);
-        tBody.appendChild(tableRow)
+        tBody.appendChild(tableRow);
     }
 }
