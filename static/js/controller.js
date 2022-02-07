1bf8607101cc38b69eb14ab2af3f1d6e6845b13e
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-02-07 14:13:19 +0100

    Add table for display in filtered actors

--- static/js/controller.js
+++ static/js/controller.js
@@ -3,10 +3,30 @@ import { dataHandler } from './data_handler.js';
 function selectionEventHandler() {
     const dropDownMenu = document.querySelectorAll("option");
     for (let option of dropDownMenu) {
-        option.addEventListener("click", (e) => {
+        option.addEventListener("click", async (e) => {
+            clearTable()
             const selection = e.target.value;
-            dataHandler.getActorsByGenre(selection).then(response => console.log(response))
+            let response = await dataHandler.getActorsByGenre(selection);
+            buildTableFromResponse(response);
         })
+    }
+}
+
+function clearTable() {
+    let tBody = document.querySelector("tbody");
+    while (tBody.hasChildNodes()) {
+        tBody.removeChild(tBody.lastChild)
+    }
+}
+
+function buildTableFromResponse(response) {
+    let tBody = document.querySelector("tbody");
+    for (let data of response) {
+        let tableRow = document.createElement("tr");
+        let tableCell = document.createElement("td");
+        tableCell.innerText = data.name;
+        tableRow.appendChild(tableCell);
+        tBody.appendChild(tableRow)
     }
 }
 
